35e52c2b3e40924fb230eb6515ad0f32ef1bcb48
EndoTomohiro <endo.t.al@m.titech.ac.jp>
2021-11-12 11:30:03 +0900

    abc169

---
+++ abc/169/d.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = int64_t;
+using pl = pair<ll, ll>;
+using vl = vector<ll>;
+using vb = vector<bool>;
+using vp = vector<pl>;
+using vs = vector<string>;
+using vvl = vector<vl>;
+using ql = queue<ll>;
+const double pi = 3.14159265359;
+const ll INF = 1000000000000000;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define rep3(i, s, n) for (ll i = (ll)(s); i < (ll)(n); i++)
+#define rep4(i, s, n) for (ll i = (ll)(n); i >= (ll)(s); i--)
+#define all(v) v.begin(), v.end()
+template<class T> void chmax(T& a, T b) {
+    if (a < b) a = b;
+}
+template<class T> void chmin(T& a, T b) {
+    if (a > b) a = b;
+}
+
+// 1 以上 N 以下の整数が素数かどうかを返す
+vector<bool> Eratosthenes(int N) {
+    // テーブル
+    vector<bool> isprime(N+1, true);
+
+    // 1 は予めふるい落としておく
+    isprime[1] = false;
+
+    // ふるい
+    for (int p = 2; p <= N; ++p) {
+        // すでに合成数であるものはスキップする
+        if (!isprime[p]) continue;
+
+        // p 以外の p の倍数から素数ラベルを剥奪
+        for (int q = p * 2; q <= N; q += p) {
+            isprime[q] = false;
+        }
+    }
+
+    // 1 以上 N 以下の整数が素数かどうか
+    return isprime;
+}
+
+int main() {
+    ll n;
+    cin >> n;
+
+    ll num = 1000000;
+    ll ans = 0;
+    vector<bool> isprime = Eratosthenes(num);
+
+    rep2(z, num) {
+        if (isprime[z] && n % z == 0) {
+            ll x = z;
+            while (n % x == 0) {
+                ans++;
+                n /= x;
+                x *= z;
+            } 
+        }
+    }
+
+    if (n > num) ans++;
+
+    cout << ans << endl;
+}
