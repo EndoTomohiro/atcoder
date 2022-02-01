fcbbb98357f80bd9ff51fd5e611c26e53a7eda5c
EndoTomohiro <endo.t.al@m.titech.ac.jp>
2022-02-01 13:14:50 +0900

    abc155

---
+++ abc/155/c.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = int64_t;
+using pl = pair<ll, ll>;
+using vl = vector<ll>;
+using vb = vector<bool>;
+using vp = vector<pl>;
+using vs = vector<string>;
+using vvl = vector<vl>;
+using ql = queue<ll>;
+using ml = map<string, ll>;
+using sl = set<ll>;
+using dl = deque<ll>;
+const double pi = 3.14159265359;
+const ll INF = 1000000000000000;
+const ll dif = 1000000007;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define rep3(i, s, n) for (ll i = (ll)(s); i < (ll)(n); i++)
+#define rep4(i, s, n) for (ll i = (ll)(n); i >= (ll)(s); i--)
+#define all(v) v.begin(), v.end()
+template<class T> void chmax(T& a, T b) {
+    if (a < b) a = b;
+}
+template<class T> void chmin(T& a, T b) {
+    if (a > b) a = b;
+}
+
+int main() {
+    ll n;
+    cin >> n;
+
+    vs s(n);
+    ml mp;
+    rep(i, n) {
+        cin >> s[i];
+        mp[s[i]]++;
+    }
+    sort(all(s));
+    s.erase(unique(all(s)), s.end());
+
+    ll a = 0;
+    rep(i, s.size()) {
+        chmax(a, mp[s[i]]);
+    }
+
+    rep(i, s.size()) {
+        if (mp[s[i]] == a) cout << s[i] << endl;
+    }
+}
