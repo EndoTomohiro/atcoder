cbfcd82e55c588b5879b0290b9cfe9340eb0d606
EndoTomohiro <endo.t.al@m.titech.ac.jp>
2022-02-19 17:20:38 +0900

    abc144

---
+++ abc/144/d.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = int64_t;
+using pl = pair<ll, ll>;
+using vl = vector<ll>;
+using vb = vector<bool>;
+using vp = vector<pl>;
+using vs = vector<string>;
+using vvl = vector<vl>;
+using ql = queue<ll>;
+using ml = map<ll, ll>;
+using sl = set<ll>;
+using dl = deque<ll>;
+const double pi = 3.14159265359;
+const ll INF = 1000000000000000;
+const ll dif = 1000000007;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define rep3(i, s, n) for (ll i = (ll)(s); i < (ll)(n); i++)
+#define rep4(i, s, n) for (ll i = (ll)(n); i >= (ll)(s); i--)
+#define all(v) v.begin(), v.end()
+template<class T> void chmax(T& a, T b) {
+    if (a < b) a = b;
+}
+template<class T> void chmin(T& a, T b) {
+    if (a > b) a = b;
+}
+
+int main() {
+    double a, b, x;
+    cin >> a >> b >> x;
+
+    cout << fixed << setprecision(11);
+    if (x * 2 >= a * a * b) {
+        cout << atan(2 * (a * a * b - x) / (a * a * a)) * 180 / pi << endl;
+    }
+    else cout << 90 - atan(2 * x / (a * b * b)) * 180 / pi << endl;
+}
