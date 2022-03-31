6fdb3d3361221ec5f6ddf7dfbe4a0807cd111d3b
EndoTomohiro <endo.t.al@m.titech.ac.jp>
2022-03-31 20:32:22 +0900

    abc126

---
+++ abc/126/b.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = int64_t;
+using pl = pair<ll, ll>;
+using vl = vector<ll>;
+using vb = vector<bool>;
+using vp = vector<pl>;
+using vs = vector<string>;
+using vc = vector<char>;
+using vvl = vector<vl>;
+using ql = queue<ll>;
+using dl = deque<ll>;
+using pri = priority_queue<ll>;
+using ml = map<ll, ll>;
+using sl = set<ll>;
+const double pi = 3.14159265359;
+const ll INF = 1000000000000000;
+const ll dif = 1000000007;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define rep3(i, s, n) for (ll i = (ll)(s); i < (ll)(n); i++)
+#define rep4(i, s, n) for (ll i = (ll)(n); i >= (ll)(s); i--)
+#define all(v) v.begin(), v.end()
+template<class T> void chmax(T& a, T b) {
+    if (a < b) a = b;
+}
+template<class T> void chmin(T& a, T b) {
+    if (a > b) a = b;
+}
+
+int main() {
+    string s;
+    cin >> s;
+    ll a = (s[0] - '0') * 10 + (s[1] - '0');
+    ll b = (s[2] - '0') * 10 + (s[3] - '0');
+    if (1 <= a && a <= 12) {
+        if (1 <= b && b <= 12) cout << "AMBIGUOUS" << endl;
+        else cout << "MMYY" << endl;
+    }
+    else {
+        if (1 <= b && b <= 12) cout << "YYMM" << endl;
+        else cout << "NA" << endl;
+    }
+}
