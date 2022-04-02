d1706eebc80254eeb0bdd4bb3ca32c30298e4d7b
EndoTomohiro <endo.t.al@m.titech.ac.jp>
2022-04-02 20:10:43 +0900

    abc125

---
+++ abc/125/c.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = int64_t;
+using pl = pair<ll, ll>;
+using vl = vector<ll>;
+using vb = vector<bool>;
+using vp = vector<pl>;
+using vs = vector<string>;
+using vc = vector<char>;
+using vvl = vector<vl>;
+using ql = queue<ll>;
+using dl = deque<ll>;
+using pri = priority_queue<ll>;
+using ml = map<ll, ll>;
+using sl = set<ll>;
+const double pi = 3.14159265359;
+const ll INF = 1000000000000000;
+const ll dif = 1000000007;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define rep3(i, s, n) for (ll i = (ll)(s); i < (ll)(n); i++)
+#define rep4(i, s, n) for (ll i = (ll)(n); i >= (ll)(s); i--)
+#define all(v) v.begin(), v.end()
+template<class T> void chmax(T& a, T b) {
+    if (a < b) a = b;
+}
+template<class T> void chmin(T& a, T b) {
+    if (a > b) a = b;
+}
+
+int main() {
+    ll n;
+    cin >> n;
+    vl a(n);
+    rep(i, n) cin >> a[i];
+
+    vl left(n), right(n);
+    left[0] = a[0];
+    rep2(i, n - 1) left[i] = gcd(left[i - 1], a[i]);
+    right[n - 1] = a[n - 1];
+    rep4(i, 0, n - 2) right[i] = gcd(right[i + 1], a[i]);
+
+    ll ans = 0;
+    rep(i, n) {
+        ll g = 0;
+        if (i) g = gcd(g, left[i - 1]);
+        g = gcd(g, right[i + 1]);
+        chmax(ans, g);
+    }
+    cout << ans << endl;
+}
