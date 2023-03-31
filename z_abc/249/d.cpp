618147585018edb3828ff73705094ff46a9bb0bb
EndoTomohiro <endo.t.al@m.titech.ac.jp>
2023-03-31 10:21:27 +0900

    abc249

---
+++ z_abc/249/d.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = int64_t;
+using pl = pair<ll, ll>;
+using vl = vector<ll>;
+using vb = vector<bool>;
+using vp = vector<pl>;
+using vs = vector<string>;
+using vc = vector<char>;
+using vvl = vector<vl>;
+using dl = deque<ll>;
+using pri = priority_queue<ll>;
+using ml = map<ll, ll>;
+using sl = set<ll>;
+const double pi = 3.14159265359;
+const ll INF = 1000000000000000;
+const ll dif = 998244353;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define rep3(i, s, n) for (ll i = (ll)(s); i <= (ll)(n); i++)
+#define rep4(i, s, n) for (ll i = (ll)(n); i >= (ll)(s); i--)
+#define all(v) v.begin(), v.end()
+template<class T> void chmax(T& a, T b) {
+    if (a < b) a = b;
+}
+template<class T> void chmin(T& a, T b) {
+    if (a > b) a = b;
+}
+
+int main() {
+    ll n;
+    cin >> n;
+    ml mp;
+    rep(i, n) {
+        ll a;
+        cin >> a;
+        mp[a]++;
+    }
+
+    ll ans = 0;
+    rep2(i, 200000) {
+        ll j = 1;
+        while (i * j <= 200000) {
+            ans += mp[i] * mp[j] * mp[i * j];
+            j++;
+        }
+    }
+    cout << ans << endl;
+}
