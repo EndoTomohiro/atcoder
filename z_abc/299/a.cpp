caccf0146888473d3c7c7e8deed773e3a3d8b4c0
EndoTomohiro <endo.t.al@m.titech.ac.jp>
2023-04-22 21:46:57 +0900

    abc299

---
+++ z_abc/299/a.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = int64_t;
+using pl = pair<ll, ll>;
+using vl = vector<ll>;
+using vb = vector<bool>;
+using vp = vector<pl>;
+using vs = vector<string>;
+using vc = vector<char>;
+using vvl = vector<vl>;
+using dl = deque<ll>;
+using pri = priority_queue<ll>;
+using ml = map<ll, ll>;
+using sl = set<ll>;
+using msl = multiset<ll>;
+const double pi = 3.14159265359;
+const ll INF = 1000000000000000;
+const ll dif = 998244353;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define rep3(i, s, n) for (ll i = (ll)(s); i < (ll)(n); i++)
+#define rep4(i, s, n) for (ll i = (ll)(n); i >= (ll)(s); i--)
+#define all(v) v.begin(), v.end()
+template<class T> void chmax(T& a, T b) {
+    if (a < b) a = b;
+}
+template<class T> void chmin(T& a, T b) {
+    if (a > b) a = b;
+}
+
+int main() {
+    ll n;
+    string s;
+    cin >> n >> s;
+    ll cnt = 0;
+    rep(i, n) {
+        if (s[i] == '*') {
+            if (cnt == 1) puts("in");
+            else puts("out");
+            return 0;
+        }
+        else if (s[i] == '|') cnt++;
+    }
+}
---
+++ z_abc/299/b.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = int64_t;
+using pl = pair<ll, ll>;
+using vl = vector<ll>;
+using vb = vector<bool>;
+using vp = vector<pl>;
+using vs = vector<string>;
+using vc = vector<char>;
+using vvl = vector<vl>;
+using dl = deque<ll>;
+using pri = priority_queue<ll>;
+using ml = map<ll, ll>;
+using sl = set<ll>;
+using msl = multiset<ll>;
+const double pi = 3.14159265359;
+const ll INF = 1000000000000000;
+const ll dif = 998244353;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define rep3(i, s, n) for (ll i = (ll)(s); i < (ll)(n); i++)
+#define rep4(i, s, n) for (ll i = (ll)(n); i >= (ll)(s); i--)
+#define all(v) v.begin(), v.end()
+template<class T> bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> void chmin(T& a, T b) {
+    if (a > b) a = b;
+}
+
+int main() {
+    ll n, t;
+    cin >> n >> t;
+    vl c(n), r(n);
+    bool ok = false;
+    rep(i, n) {
+        cin >> c[i];
+        if (c[i] == t) ok = true;
+    }
+    rep(i, n) cin >> r[i];
+
+    if (!ok) t = c[0];
+    ll ans = 0, maxr = 0;
+    rep(i, n) {
+        if (c[i] == t) {
+            if (chmax(maxr, r[i])) ans = i + 1;
+        }
+    }
+    cout << ans << endl;
+}
